9fe2a9e84caed75b8e8fb8d8ecec779bbc738e5f
Dante Dalla Gasperina <danted@microsoft.com>
2023-11-02 17:38:09 -0700

    renames

--- src/Resources/Stacks.Autorest/custom/helpers/ConvertTagsObjectToHashtable.ps1
+++ src/Resources/Stacks.Autorest/custom/helpers/ConvertTagsObjectToHashtable.ps1
@@ -15,7 +15,7 @@
 function ConvertTagsObjectToHashtable {
     [OutputType([hashtable])]
     [CmdletBinding(PositionalBinding)]
-    # [Microsoft.Azure.PowerShell.Cmdlets.Resources.DeploymentStacks.DoNotExportAttribute()]
+    [Microsoft.Azure.PowerShell.Cmdlets.Resources.DeploymentStacks.DoNotExportAttribute()]
     [Microsoft.Azure.PowerShell.Cmdlets.Resources.DeploymentStacks.Description("Converts generated Tags object into hashtable.")]
     param(
         [Parameter(Position = 0, Mandatory)]
